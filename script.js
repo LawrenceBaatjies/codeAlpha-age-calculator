7962bbf9a03734e1d2a1a50487a44dc80316b5f9
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2024-03-11 21:12:29 +0200

    Update script.js
    added querySelectors

---
+++ script.js
@@ -0,0 +1,7 @@
+// get input fields and result elements
+const dayInput = document.querySelector('.get-info .day input');
+const monthInput = document.querySelector('.get-info .month input');
+const yearInput = document.querySelector('.get-info .year input');
+const yearResult = document.querySelector('.result h3:nth-of-type(1) span');
+const monthResult = document.querySelector('.result h3:nth-of-type(2) span');
+const dayResult = document.querySelector('.result h3:nth-of-type(3) span');
