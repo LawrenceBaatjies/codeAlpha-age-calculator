214115ebdb2f02689f34c99c70ef0c5bcaaae550
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2024-03-11 21:17:51 +0200

    Update script.js
    eventListener added

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const yearInput = document.querySelector('.get-info .year input');
 const yearResult = document.querySelector('.result h3:nth-of-type(1) span');
 const monthResult = document.querySelector('.result h3:nth-of-type(2) span');
 const dayResult = document.querySelector('.result h3:nth-of-type(3) span');
+
+// add event listener to the calculate button.
+
+let calcButton = document.querySelector('.middle-line .calc');
+calcButton.addEventListener('click', (event) => {
+    event.preventDefault();  // prevent the default behavior of the button
+    calculateAge();
+})
+
+// add event listener to the input fields
+dayInput.addEventListener('click', () => {
+    if(event.keyCode === 13) {
+        
+    }
+})
