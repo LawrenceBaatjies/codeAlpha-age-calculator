a197f7723fd208cc3ce40799c7ffa0979c1a29e3
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2024-03-27 22:52:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -91,4 +91,48 @@ const calculateAge = () => {
   dayResult.textContent = ageDays;
 
   
+};
+
+
+// CalculateStarSign 
+const calculateStarSign = () => {
+  const birthDate = new Date(document.getElementById('birthdate').value);
+  const month = birthDate.getMonth() + 1;
+  const day = birthDate.getDate();
+
+  let starSign = '';
+
+      if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) {
+        starSign = 'Aries';
+    } else if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) {
+        starSign = 'Taurus';
+    } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+        starSign = 'Gemini';
+    } else if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) {
+        starSign = 'Cancer';
+    } else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
+        starSign = 'Leo';
+    } else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {
+        starSign = 'Virgo';
+    } else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
+        starSign = 'Libra';
+    } else if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) {
+        starSign = 'Scorpio';
+    } else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
+        starSign = 'Sagittarius';
+    } else if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) {
+        starSign = 'Capricorn';
+    } else if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) {
+        starSign = 'Aquarius';
+    } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+        starSign = 'Pisces';
+    }
+
+  document.getElementById('result').innerText = `Your star sign is: ${starSign}`;
+
+  
 }
+
+ 
+
+
